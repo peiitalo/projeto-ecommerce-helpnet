df84c491abe47efe7018ca69754fdcd5c1064261
peiitalo <pedroitalo609@gmail.com>
2025-10-20 15:06:17 -0300

    feat(admin) admin page added

---
+++ frontend/src/pages/admin/AdminLogin.jsx
@@ -0,0 +1,10 @@
+function AdminLogin() {
+    return (
+        <div>
+        <h1>Admin Login Page</h1>
+        {/* Formulário de login do administrador */}
+        </div>
+    );
+    }
+
+export default AdminLogin;
